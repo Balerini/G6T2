b67a25846941ede2509e767dd6d0d389c88237c0
Ng Mei Jia <ngvaleriemj@gmail.com>
2025-10-17 01:06:24 +0800

    SCRUM-79 [Frontend] Only owner can delete task

--- frontend/src/views/ViewTaskDetails.vue
+++ frontend/src/views/ViewTaskDetails.vue
@@ -104,8 +104,13 @@
               <button @click="openSubtaskModal" class="add-subtask-btn">
                 + Add Subtask
               </button>
-              <button @click="confirmDeleteTask" class="delete-task-btn-icon" v-if="task && !task.is_deleted" title="Delete Task">
-                  <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+              <!-- Delete Task Button - Only show for task owner -->
+              <button 
+                  @click="confirmDeleteTask" 
+                  class="delete-task-btn" 
+                  v-if="task && !task.is_deleted && isTaskOwner"
+              >
+                  <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                       <polyline points="3,6 5,6 21,6"></polyline>
                       <path d="m19,6v14a2,2 0 0,1 -2,2H7a2,2 0 0,1 -2,-2V6m3,0V4a2,2 0 0,1 2,-2h4a2,2 0 0,1 2,2v2"></path>
                       <line x1="10" y1="11" x2="10" y2="17"></line>
